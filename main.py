08e658df19a8176d74a2ca8bd98c0de9a2ebe4db
alegomgu <61535863+alegomgu@users.noreply.github.com>
2023-11-23 15:53:13 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from app.routes.hello import router as hello_router
+import os
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+# Incluir el router del endpoint Hello World
+app.include_router(hello_router)
+
+if __name__ == "__main__":
+    # Aquí se podría incluir una función 'configure()' si es necesaria para configurar la app
+    uvicorn.run(app, host=os.getenv("SERVICE_URL"), port=int(os.getenv("SERVICE_PORT")))
